cfd370663bc01c4f0db0c4b7b71f05af0888f93e
mehmetsungur <mehmetsungur90@gmail.com>
2025-08-26 11:04:37 +0300

    fix

--- main.tf
+++ main.tf
@@ -2,8 +2,60 @@ provider "aws" {
   region = "eu-north-1"
 }
 
+# VPC
+resource "aws_vpc" "my-vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "my-vpc"
+  }
+}
+
+# Subnet
+resource "aws_subnet" "my-public-subnet" {
+  vpc_id                  = aws_vpc.my-vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-north-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "my-public-subnet"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "my-igw" {
+  vpc_id = aws_vpc.my-vpc.id
+
+  tags = {
+    Name = "my-igw"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "my-public-rt" {
+  vpc_id = aws_vpc.my-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my-igw.id
+  }
+
+  tags = {
+    Name = "my-public-rt"
+  }
+}
+
+# Route Table Association
+resource "aws_route_table_association" "my-public-assoc" {
+  subnet_id      = aws_subnet.my-public-subnet.id
+  route_table_id = aws_route_table.my-public-rt.id
+}
+
 # Security Group - for SSH access only
-resource "aws_security_group" "ssh_sg" {
+resource "aws_security_group" "demo-instance-sg" {
   name        = "allow-ssh"
   description = "Allow SSH inbound traffic"
   vpc_id      = "vpc-0511d5dc197f947a5" # You should enter your own VPC ID here.
@@ -24,17 +76,19 @@ resource "aws_security_group" "ssh_sg" {
   }
 
   tags = {
-    Name = "allow-ssh"
+    Name = "demo-instance-sg"
   }
 }
 
 resource "aws_instance" "demo-instance" {
-  ami           = "ami-0c4fc5dcabc9df21d" # Amazon Linux 2 AMI (HVM), SSD Volume Type
+  ami           = "ami-0a716d3f3b16d290c" # Ubuntu Server 24.04 LTS (HVM),EBS General Purpose (SSD) Volume Type.
   instance_type = "t3.micro"
   key_name      = "project" # Ensure you have created this key pair in the AWS console
 
-  vpc_security_group_ids = [aws_security_group.ssh_sg.id]
+  vpc_security_group_ids = [aws_security_group.demo-instance-sg.id]
+
+  for_each = toset(["Jenkins-master", "build-worker", "ansible"])
   tags = {
-    Name = "demo-instance"
+    Name = "${each.key}-instance"
   }
 }
